244640631be6e289c00af94ab476302baed15ed2
ochki <nganaa26@gmail.com>
2021-11-17 13:05:56 +0800

    gridlis added

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -13,46 +13,26 @@ class ProductList extends StatefulWidget {
 class _ProductListState extends State<ProductList> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Card(
-                            child: Container(
-                              height: 150,
-                              width: 150,
-                              color: Colors.black,
-                              child: Column(
-                                children: [
-                                  Container(
-                                    height: 120,
-                                    width: 130,
-                                    decoration: const BoxDecoration(
-                                        color: Colors.white),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
+    const title = 'Grid List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          children: List.generate(10, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline5,
               ),
-            )
-          ],
+            );
+          }),
         ),
       ),
     );
