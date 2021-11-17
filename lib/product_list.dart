929231ffe23a268d7c0f0073ffad1dcbed8d554b
amigosilo <amirlinb@gmail.com>
2021-11-17 13:40:14 +0800

    Read data from firestore database.

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class ProductList extends StatefulWidget {
   const ProductList({Key? key, required this.title}) : super(key: key);
@@ -11,6 +13,7 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,31 +26,59 @@ class _ProductListState extends State<ProductList> {
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Card(
-                            child: Container(
-                              height: 150,
-                              width: 150,
-                              color: Colors.black,
-                              child: Column(
-                                children: [
-                                  Container(
-                                    height: 120,
-                                    width: 130,
-                                    decoration: const BoxDecoration(
-                                        color: Colors.white),
-                                  )
-                                ],
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Card(
+                                child: Container(
+                                  height: 150,
+                                  width: 150,
+                                  color: Colors.black,
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        height: 120,
+                                        width: 130,
+                                        decoration: const BoxDecoration(
+                                            color: Colors.white
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
-                            ),
+                              StreamBuilder(
+                                stream: FirebaseFirestore.instance.collection('product_list').snapshots(),
+                                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                                  if (!snapshot.hasData) {
+                                    return const Center(
+                                      child: CircularProgressIndicator(),
+                                    );
+                                  }
+
+                                  return Expanded(
+                                    child: SizedBox(
+                                      height: 200,
+                                      child: ListView(
+                                        children: snapshot.data!.docs.map((document) {
+                                          return Center(
+                                            child: Text(document['name']),
+                                          );
+                                        }).toList(),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              )
+                            ],
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   )
                 ],
               ),
