d46bc78e4613bf787ad7f9d314adbc5d1c92aea9
NEnhbileg <90662621+NEnhbileg@users.noreply.github.com>
2021-12-12 16:33:58 +0800

    Horizontal Listview

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -15,6 +15,7 @@ class _ProductListState extends State<ProductList> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
@@ -50,6 +51,35 @@ class _ProductListState extends State<ProductList> {
                                   ),
                                 ),
                               ),
+                              Container(
+                                margin: EdgeInsets.symmetric(vertical: 20.0),
+                                height: 200,
+                                child: ListView(
+                                  scrollDirection: Axis.horizontal,
+                                    children: <Widget>[
+                                      Container(width: 160.0,
+                                      child:Card(
+                                        child:Wrap(
+                                          children: <Widget>[
+                                            Image.network("https://www.google.com/imgres?imgurl=https%3A%2F%2Fmiro.medium.com%2Fmax%2F1200%2F1*mk1-6aYaf_Bes1E3Imhc0A.jpeg&imgrefurl=https%3A%2F%2Ftowardsdatascience.com%2F3-numpy-image-transformations-on-baby-yoda-c27c1409b411&tbnid=gOUAFhrbQ2nYQM&vet=12ahUKEwjGsJ_U5930AhUUgMYKHR-EBKsQMygQegUIARDqAQ..i&docid=OXvyXJop1qSGqM&w=1200&h=900&itg=1&q=image&ved=2ahUKEwjGsJ_U5930AhUUgMYKHR-EBKsQMygQegUIARDqAQ"),
+                                            ListTile(
+                                              title: Text("Heading1"),
+                                              subtitle: Text("Subheading1"),
+                                            )
+                                          ],
+                                        ),
+                                ),
+                                      )
+                                    ],
+
+                                ),
+
+                              ), //First card
+
+
+
+
+
                               StreamBuilder(
                                 stream: FirebaseFirestore.instance.collection('product_list').snapshots(),
                                 builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
