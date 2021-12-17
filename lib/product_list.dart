8142bd6dafc8bac25634ad695e49129c1fd80a5d
amigosilo <amirlinb@gmail.com>
2021-12-17 19:47:07 +0800

    Updated product_list.dart to grid, product_info.dart accepts document parameters

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -29,75 +29,79 @@ class _ProductListState extends State<ProductList> {
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
-                children:[
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              StreamBuilder(
-                                stream: FirebaseFirestore.instance.collection('product_list').snapshots(),
-                                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                                  if (!snapshot.hasData) {
-                                    return const Center(
-                                      child: CircularProgressIndicator(),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  StreamBuilder(
+                    stream: FirebaseFirestore.instance.collection('product_list').snapshots(),
+                    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return Flexible(
+                        child: GridView.count(
+                          childAspectRatio: 0.7,
+                          crossAxisCount: 2,
+                          children: snapshot.data!.docs.map((document) {
+                            return Column(
+                              children: [
+                                Image.asset(document['img_path'],
+                                  height: 200,
+                                  width: 200,
+                                  fit:  BoxFit.fitWidth,
+                                ),
+                                TextButton(
+                                  child: Text(
+                                      document['name'],
+                                      style: const TextStyle(fontSize: 19)
+                                  ),
+                                  style: ButtonStyle(
+                                      padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(13)),
+                                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                                      backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(18.6),
+                                          )
+                                      )
+                                  ),
+                                  onPressed: () {
+                                    // prefs.setString('product_id', document['id']);
+                                    Navigator.pushNamed(
+                                        context, '/product_info',
+                                        arguments: Doc(
+                                          document.id,
+                                          document['name'],
+                                          document['description'],
+                                          document['img_path'],
+                                          document['price'],
+                                        ),
                                     );
-                                  }
-
-                                  return Expanded(
-                                    child: SizedBox(
-                                      height: 200,
-                                      child: ListView(
-                                        children: snapshot.data!.docs.map((document) {
-                                          return Center(
-                                            child: Column(
-                                              children: [
-                                                Image.asset(document['img_path'],
-                                                  height: 200,
-                                                  width: 200,
-                                                  fit:  BoxFit.fitWidth,
-                                                ),
-                                                TextButton(
-                                                  child: Text(
-                                                      document['name'],
-                                                      style: TextStyle(fontSize: 19)
-                                                  ),
-                                                  style: ButtonStyle(
-                                                      padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(13)),
-                                                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                                                      backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                                          RoundedRectangleBorder(
-                                                            borderRadius: BorderRadius.circular(18.6),
-                                                          )
-                                                      )
-                                                  ),
-                                                  onPressed: () {
-                                                    // prefs.setString('product_id', document['id']);
-                                                    Navigator.pushNamed(context, '/product_info');
-                                                  },
-                                                ),
-                                              ],
-                                            ),
-                                          );
-                                        }).toList(),
-                                      ),
-                                    ),
-                                  );
-                                },
-                              )
-                            ],
-                          ),
+                                  },
+                                ),
+                              ],
+                            );
+                          }).toList(),
                         ),
-                      ],
-                    ),
-                  ),
-              ]),
+                      );
+                    },
+                  )
+                ],
+              ),
             ),
           ]),
         ),
       );
   }
+}
+
+class Doc {
+  String id;
+  String name;
+  String description;
+  String imgPath;
+  int price;
+
+  Doc(this.id, this.name, this.description, this.imgPath, this.price);
 }
