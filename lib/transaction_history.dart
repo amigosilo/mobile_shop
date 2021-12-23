3b8907dd544c7e0f1c4f860f5354037a88a0d489
Name of your next sex tape <s18ud1120044@nmit.edu.mn>
2021-12-23 16:00:39 +0800

    rollback

--- lib/transaction_history.dart
+++ lib/transaction_history.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+class TransactionHistory extends StatefulWidget {
+  const TransactionHistory({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<TransactionHistory> createState() => _TransactionHistoryState();
+}
+
+class _TransactionHistoryState extends State<TransactionHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
